8e12b4b5bd1dd3058caaf1b476b0ba3e3540189a
p4th4k <157967122+p4th4k@users.noreply.github.com>
2025-02-09 18:32:44 +0530

    Added confetti and stuff

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ const gifSrc = [
   "./gifs/boxing-fight.gif",
   "./gifs/laugh-laughing.gif",
   "./gifs/hmpf-hmph.gif",
-  "./gifs/cute-funny.gif"
+  "./gifs/cute-funny.gif",
 ];
 
 const audioSrc = [
@@ -27,8 +27,18 @@ const audioSrc = [
   "./mp3/no1.mp3",
   "./mp3/no2.mp3",
   "./mp3/no4.mp3",
-  "./mp3/no5.mp3"
+  "./mp3/no5.mp3",
 ];
+
+const defaults = {
+  spread: 360,
+  ticks: 100,
+  gravity: 0,
+  decay: 0.94,
+  startVelocity: 30,
+  shapes: ["heart"],
+  colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+};
 
 const btnNo = document.querySelector(".no-button");
 const btnYes = document.querySelector(".yes-button");
@@ -48,20 +58,37 @@ const handleNo = () => {
 
   gifImg.src = gifSrc[gifIndex];
   gifIndex = (gifIndex + 1) % gifSrc.length;
-  
-    if(currentAudio){
-        currentAudio.pause()
-        currentAudio.time = 0;
-    }
+
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.time = 0;
+  }
 
   currentAudio = new Audio(audioSrc[audioIndex]);
   audioIndex = (audioIndex + 1) % audioSrc.length;
   currentAudio.volume = 0.2;
   currentAudio.play();
 
-  currentAudio.onended = () => currentAudio = null;
+  currentAudio.onended = () => (currentAudio = null);
 };
 
 const handleYes = () => {
-  window.location.href = "./yes_page/yes_page.html";
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 3,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 4,
+  });
+  setTimeout(() => window.location.href = "./yes_page/yes_page.html", 5000);
 };
---
+++ js/yes.js
@@ -0,0 +1,40 @@
+document.addEventListener("click", () => {
+  let yesAudio = new Audio("../mp3/yes.mp3");
+  yesAudio.volume = 0.5;
+  yesAudio.play();
+});
+
+const duration = 3600 * 1000,
+  animationEnd = Date.now() + duration;
+
+let skew = 1;
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+(function frame() {
+  const timeLeft = animationEnd - Date.now(),
+    ticks = Math.max(200, 500 * (timeLeft / duration));
+
+  skew = Math.max(0.8, skew - 0.001);
+
+  confetti({
+    particleCount: 1,
+    startVelocity: 0,
+    ticks: ticks,
+    origin: {
+      x: Math.random(),
+      y: Math.random() * skew - 0.2,
+    },
+    colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+    shapes: ["heart"],
+    gravity: randomInRange(0.4, 0.6),
+    scalar: randomInRange(1, 5),
+    drift: randomInRange(-0.4, 0.4),
+  });
+
+  if (timeLeft > 0) {
+    requestAnimationFrame(frame);
+  }
+})();
